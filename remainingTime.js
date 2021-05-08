ab70e309e9cee30c4577f5bfd6e4d56bb5d18e14
Aryan Jha <aryanjha@gmail.com>
2021-05-08 01:27:44 -0400

    timer working

--- remainingTime.js
+++ remainingTime.js
@@ -10,12 +10,43 @@ function hideButtons() {
     document.getElementById("time-select").remove();
 }
 
-// create function to act 
+
+function resetTimer(){
+    // create buttons
+    
+    // call addEventListener to Buttons
+}
+
+
+
 function beginCountDown(startingTime) {
     // here goes code for making a timer
+    var element = document.getElementById("countdown-timer")
 
+    var countdown = parseInt(startingTime) * 60 * 1000; // time in milliseconds
+    var timer = setInterval(function(){
+        countdown -= 1000;
+        var min = Math.floor(countdown / (60 * 1000));
+        var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);
 
-    document.getElementById("countdown-timer").innerHTML = startingTime;
+        if (countdown <= 0) {
+            alert("Break Time");
+            clearInterval(timer);
+            // remove timer, re-add buttons
+            element.innerHTML = "";
+            
+        } else {
+            if (sec < 10) {
+                element.innerHTML = (min + "m : 0" + sec + "s");
+            }
+            else {
+                element.innerHTML = (min + "m : " + sec + "s");
+            }
+            
+        }
+    }, 1000); // execute this function every 1000 milliseconds AKA 1 second
+
+    
 }
 
 
